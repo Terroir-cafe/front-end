34c52ca1dc635ff430db6a4c96b077ff751e963b
gustavoalvhnr <gustavoalves.ho@gmail.com>
2026-04-17 19:38:19 -0300

    Feat-1: #1 Criação da HomeView e integração com o back-end

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import './assets/css/global.css'
+import 'vue-loading-overlay/dist/css/index.css'
 
 import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(createPinia())
 app.mount('#app')
