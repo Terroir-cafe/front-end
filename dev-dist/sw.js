c5c63f9b33eac765f7b120f39d9a50f7dc11bcff
gustavoalvhnr <gustavoalves.ho@gmail.com>
2026-05-25 18:07:54 -0300

    refactor/ Limpeza de componentes não utilizados na app.vue

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.vaoo7be7u4g"
+    "revision": "0.l21apn914r8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import AppCadastro from './components/forms/appCadastro.vue';
-import appFooter from './components/layout/appFooter.vue';
-import SobreNosView from './views/SobreNosView.vue';
 </script>
 
 <template>
   <RouterView />
-  <AppCadastro/>
-  <app-footer />
-  <SobreNosView />
 </template>
 
 <style scoped>
